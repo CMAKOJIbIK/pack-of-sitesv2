60d2ac9a1e50d39d83e368f19917c4b6bb06be08
CMAROJIbIK <kvegrenovskij@gmail.com>
2023-01-05 13:44:42 +0300

    o

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body>
-    <div class="content"></div>
+    <div class="content">
+        <h1>tttttttttt</h1>
+    </div>
 </body>
 </html>
