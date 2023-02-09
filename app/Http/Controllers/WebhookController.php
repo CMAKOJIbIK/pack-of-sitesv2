c41ade8ea89d5a3574a0135bd28d3566deabe175
CMAROJIbIK <kvegrenovskij@gmail.com>
2023-02-09 16:58:14 +0300

    o

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -54,7 +54,7 @@ class WebhookController extends Controller
         if($text == "message"){
             $this->telegram->send_message(5057038547, "hi");
         }
-        $this->telegram->send_buttons(5057038547, "Задание 1");
+        $this->telegram->send_buttons(5057038547, "Задание 1", 1);
     }
 
     public function test(){
